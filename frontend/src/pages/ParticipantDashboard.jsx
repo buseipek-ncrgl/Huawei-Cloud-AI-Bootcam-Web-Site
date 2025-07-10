32fa9519b2e22006baa5b42a34a639833203a17d
buseipek-ncrgl <ipekncrgl@gmail.com>
2025-07-11 00:57:28 +0300

    update

--- frontend/src/pages/ParticipantDashboard.jsx
+++ frontend/src/pages/ParticipantDashboard.jsx
@@ -111,7 +111,9 @@ const ParticipantDashboard = () => {
       className="w-40 sm:w-48 drop-shadow-2xl brightness-125"
     />
   </div>
-  <h1 className="text-3xl font-bold text-yellow-400">Huawei Cloud AI Bootcamp</h1>
+  <h1 className="text-2xl sm:text-2xl font-extrabold text-yellow-400 tracking-wide drop-shadow-lg text-center">
+    Huawei Cloud AI Bootcamp
+  </h1>
 </div>
 
       {/* ANA LAYOUT */}
